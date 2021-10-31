6322611fe0e5aaa5a76942ab0d4a1be85584643e
Marcin Sikora <memor37@onet.pl>
2021-11-01 00:44:12 +0100

    Added UI and play button logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import './App.css';
 import Display from './components/Display';
 import PropertiesList from './components/PropertiesList';
+import UI from './components/ui/UI';
 
 export class App extends Component
 {
@@ -10,7 +11,10 @@ export class App extends Component
 	{
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
+		this.togglePlay = this.togglePlay.bind(this);
+
 		this.state = {
+			isRunning: true,
 			properties: {
 				background: {
 					color: "#000"
@@ -27,9 +31,10 @@ export class App extends Component
 					amount: 1
 				},
 				particle: {
-					shape: 0,
+					image: 0,
 					color: "#fff",
 					scale: 1,
+					speed: 1,
 					lifespan: 1,
 					emissionColor: "#fff",
 					emissionRadius: 1
@@ -47,11 +52,19 @@ export class App extends Component
 		})
 	}
 
+	togglePlay()
+	{
+		this.setState(state => {
+			return {isRunning: !state.isRunning}
+		});
+	}
+
 	render()
 	{
 		return (
 			<div className="App">
-				<Display properties={this.state.properties}></Display>
+				<UI togglePlay={this.togglePlay}></UI>
+				<Display isRunning={this.state.isRunning} properties={this.state.properties}></Display>
 				<PropertiesList onChange={this.handleChange}></PropertiesList>
 			</div>
 		);
