f1c51909248a6eac54eabc8016947b7347a3c6d7
Marcin Sikora <memor37@onet.pl>
2021-11-03 12:40:32 +0100

    Removed unnecessary comment

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 import './Property.css'
 export class Property extends Component {
-    // constructor(props)
-    // {
-    //     super(props);
-    // }
-    
     render() {
         return (
             <div className="Property">
