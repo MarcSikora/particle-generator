eb917f745ee0e3cc6e767884aa2418458cdc648c
Marcin Sikora <memor37@onet.pl>
2021-11-04 22:06:07 +0100

    Added manual

--- src/components/ui/Info.js
+++ src/components/ui/Info.js
@@ -5,8 +5,28 @@ export class Info extends Component {
     render() {
         return (
             <div className="Info">
-                Particle Systems: {this.props.particleSystemsCount} | 
-                Objects: {this.props.objectsCount} 
+                <div class="Info--manual">
+                    <div class="Info--manual-item">
+                        <span class="Info--manual-key">LMB</span>
+                        <span class="Info--manual-description"> - grab/move/select object</span>
+                    </div>
+                    <div class="Info--manual-item">
+                        <span class="Info--manual-key">X</span>
+                        <span class="Info--manual-description"> - remove object</span>
+                    </div>
+                    <div class="Info--manual-item">
+                        <span class="Info--manual-key">W/ArrowUp</span>
+                        <span class="Info--manual-description"> - move object layer up</span>
+                    </div>
+                    <div class="Info--manual-item">
+                        <span class="Info--manual-key">S/ArrowUp</span>
+                        <span class="Info--manual-description"> - move object layer down</span>
+                    </div>
+                </div>
+                <div class="Info--counters">
+                    Particle Systems: {this.props.particleSystemsCount} | 
+                    Objects: {this.props.objectsCount} 
+                </div>
             </div>
         )
     }
