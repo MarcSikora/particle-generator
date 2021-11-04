88e5d4a5f80e37f4a2b56d539862d8f2df6509ae
Marcin Sikora <memor37@onet.pl>
2021-11-04 22:07:11 +0100

    Added new images

--- src/graphics/ImageManager.js
+++ src/graphics/ImageManager.js
@@ -4,14 +4,19 @@ import triangleImg from '../assets/triangle.png'
 
 import snowflake_01 from '../assets/snowflake_01.png';
 import snowflake_02 from '../assets/snowflake_02.png';
+import snowflake_03 from '../assets/snowflake_03.png';
 import smoke_01 from '../assets/smoke_01.png';
 import smoke_02 from '../assets/smoke_02.png';
 import smoke_03 from '../assets/smoke_03.png';
+import drop_01 from '../assets/drop_01.png';
+import drop_02 from '../assets/drop_02.png';
+import drop_03 from '../assets/drop_03.png';
 
 import cloud_01 from '../assets/cloud_01.png';
 import cloud_02 from '../assets/cloud_02.png';
 import tree_01 from '../assets/tree_01.png';
 import house_01 from '../assets/house_01.png';
+import chimney_01 from '../assets/chimney_01.png';
 
 class ImageManager
 {
@@ -26,16 +31,21 @@ class ImageManager
         this.particleImages = [
             snowflake_01,
             snowflake_02,
+            snowflake_03,
             smoke_01,
             smoke_02,
-            smoke_03
+            smoke_03,
+            drop_01,
+            drop_02,
+            drop_03,
         ];
 
         this.objectImages = [
             cloud_01,
             cloud_02,
             tree_01,
-            house_01
+            house_01,
+            chimney_01
         ];
     }
 
