b3157ee086d884a03c750a079fb5a61c522d329e
Marcin Sikora <memor37@onet.pl>
2021-11-05 09:45:24 +0100

    Add manual to Info

--- src/components/ui/Info.js
+++ src/components/ui/Info.js
@@ -5,25 +5,25 @@ export class Info extends Component {
     render() {
         return (
             <div className="Info">
-                <div class="Info--manual">
-                    <div class="Info--manual-item">
-                        <span class="Info--manual-key">LMB</span>
-                        <span class="Info--manual-description"> - grab/move/select object</span>
+                <div className="Info--manual">
+                    <div className="Info--manual-item">
+                        <span className="Info--manual-key">LMB</span>
+                        <span className="Info--manual-description"> - grab/move/select object</span>
                     </div>
-                    <div class="Info--manual-item">
-                        <span class="Info--manual-key">X</span>
-                        <span class="Info--manual-description"> - remove object</span>
+                    <div className="Info--manual-item">
+                        <span className="Info--manual-key">X</span>
+                        <span className="Info--manual-description"> - remove object</span>
                     </div>
-                    <div class="Info--manual-item">
-                        <span class="Info--manual-key">W/ArrowUp</span>
-                        <span class="Info--manual-description"> - move object layer up</span>
+                    <div className="Info--manual-item">
+                        <span className="Info--manual-key">W/ArrowUp</span>
+                        <span className="Info--manual-description"> - move object layer up</span>
                     </div>
-                    <div class="Info--manual-item">
-                        <span class="Info--manual-key">S/ArrowUp</span>
-                        <span class="Info--manual-description"> - move object layer down</span>
+                    <div className="Info--manual-item">
+                        <span className="Info--manual-key">S/ArrowUp</span>
+                        <span className="Info--manual-description"> - move object layer down</span>
                     </div>
                 </div>
-                <div class="Info--counters">
+                <div className="Info--counters">
                     Particle Systems: {this.props.particleSystemsCount} | 
                     Objects: {this.props.objectsCount} 
                 </div>
